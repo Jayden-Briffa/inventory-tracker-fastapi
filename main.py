8eb45cb8ed2ea8dbdacc09339fb70e59531aecb3
jayden.briffa <jayden.briffa@vodafone.com>
2026-02-25 11:47:23 +0000

    Implement front/backend split POC

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Request
 import models
 from db import get_db
 from sqlalchemy.orm import Session
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+apiRoute = "/api/"
+viewRoute = "/"
 
-@app.post("/items/", response_model=models.ItemResponse)
+templates = Jinja2Templates(directory="templates")
+@app.get(f"{viewRoute}", response_class=HTMLResponse)
+async def page_home(request: Request):
+    return templates.TemplateResponse("index.html", {
+        "request": request,
+        "message": "Hello, world!",
+    })
+
+@app.post(f"{apiRoute}items/", response_model=models.ItemResponse)
 async def create_item(item: models.ItemCreate, db: Session = Depends(get_db)):
     db_item = models.Item(**item.model_dump())
     db.add(db_item)
